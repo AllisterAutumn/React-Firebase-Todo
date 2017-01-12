668eff5ff88f4a4edab4aedd8ddac85cfc35707e
val rigodon <rvrigodon@gmail.com>
2017-01-12 12:09:17 -0500

    User can view a todo

--- src/App.js
+++ src/App.js
@@ -63,39 +63,8 @@ class App extends Component {
       </div>
     );
   }
-
-  renderTodoList() {
-    let todoElements = [];
-
-    for(let todoId in this.state.todos) {
-      let todo = this.state.todos[todoId]
-
-      todoElements.push(
-        <div className="todo d-flex justify-content-between pb-4" key={todoId}>
-          <div className="mt-2">
-            <h4>{todo.title}</h4>
-            <div>{moment(todo.createdAt).calendar()}</div>
-          </div>
-          <button
-            className="ml-4 btn btn-link"
-            onClick={ () => { this.deleteTodo(todoId) } }
-          >
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-      );
-    }
-
-
-    return (
-      <div className="todo-list">
-        {todoElements}
-      </div>
-    );
-  }
-
-   deleteTodo (todoId) {
-axios({
+deleteTodo (todoId) {
+  axios({
       url: `/todos/${todoId}.json`,
       baseURL: 'https://star-wars-routing.firebaseio.com',
       method: "DELETE"
@@ -109,13 +78,64 @@ axios({
   }
 
 
+   renderTodoList() {
+    let todoElements = [];
+
+    for(let todoId in this.state.todos) {
+      let todo = this.state.todos[todoId]
+
+      todoElements.push(
+        <div className="todo d-flex justify-content-between pb-4" key={todoId}>
+          <div className="mt-2" onClick={ () => this.selectTodo(todoId) }>
+            <h4>{todo.title}</h4>
+            <div>{moment(todo.createdAt).calendar()}</div>
+          </div>
+          <button
+            className="ml-4 btn btn-link"
+            onClick={ () => { this.deleteTodo(todoId) } }
+          >
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+      );
+    }
+
+    return (
+      <div className="todo-list">
+        {todoElements}
+      </div>
+    );
+  }
+
+  selectTodo(todoId) {
+    this.setState({ currentTodo: todoId });
+  }
+
+  renderSelectedTodo() {
+    let content;
+
+    if (this.state.currentTodo) {
+      let currentTodo = this.state.todos[this.state.currentTodo];
+      content =  (
+        <div>
+          <h1>{currentTodo.title}</h1>
+        </div>
+      );
+    }
+
+    return content;
+  }
+
   render() {
     return (
       <div className="App container-fluid">
         <div className="row pt-3">
           <div className="col-6 px-4">
-           {this.renderNewTodoBox()}
+            {this.renderNewTodoBox()}
             {this.renderTodoList()}
+          </div>
+          <div className="col-6 px-4">
+            {this.renderSelectedTodo()}
           </div>
         </div>
       </div>
